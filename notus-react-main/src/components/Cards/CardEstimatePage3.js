512f88dc194cb719d1b2cc9aa37cefa8cf1be38a
Bhashana Thennakoon <147265731+BhashanaThennakoon99@users.noreply.github.com>
2025-04-27 23:04:13 +0530

    first commit

--- notus-react-main/src/components/Cards/CardEstimatePage3.js
+++ notus-react-main/src/components/Cards/CardEstimatePage3.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import TreeView from "./TreeView";
 
@@ -9,17 +10,7 @@ function CardEstimatePage3({ formData, errors, onBack, onSubmit, onInteraction }
           <div className="w-full lg:w-12/12 px-4">
             <div className="relative w-full mb-3">
               <div className="pegging-label">
-
-                {/* <button
-                  type="button"
-                  className="bg-emerald-400 text-white text-sm px-4 py-2 rounded shadow hover:shadow-md"
-                  onClick={toggleTreeView}
-                >
-                  {showTreeView ? "Hide Material" : "Show Material"}
-                </button> */}
-
-                <h3 className="text-lg font-bold text-blueGray-700 mb-2">Material Tree</h3>
-
+                <h3 className="text-lg font-bold text-blueGray-700 mb-2">Material Stratification</h3>
               </div>
               <TreeView onInteraction={onInteraction} />
             </div>

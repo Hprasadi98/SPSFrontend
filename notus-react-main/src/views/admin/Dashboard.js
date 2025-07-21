7d6416e38577df80f24941f52113a1db7918862b
HarithaNawarathna <nawarathnammhr.21@uom.lk>
2025-07-21 11:31:14 +0530

    chart.js

--- notus-react-main/src/views/admin/Dashboard.js
+++ notus-react-main/src/views/admin/Dashboard.js
@@ -6,26 +6,35 @@ import CardLineChart from "components/Cards/CardLineChart.js";
 import CardBarChart from "components/Cards/CardBarChart.js";
 import CardPageVisits from "components/Cards/CardPageVisits.js";
 import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+import CardPieChart from "components/Cards/CardPieChart.js";
 
 export default function Dashboard() {
   return (
     <>
+
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardLineChart />
         </div>
         <div className="w-full xl:w-4/12 px-4">
           <CardBarChart />
+        </div>        
+      </div>
+
+        <div className="flex flex-wrap">
+        <div className="w-full xl:w-6/12 px-4">
+          <CardPieChart />
         </div>
       </div>
-      <div className="flex flex-wrap mt-4">
+
+      {/* <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardPageVisits />
         </div>
         <div className="w-full xl:w-4/12 px-4">
           <CardSocialTraffic />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
